4e6712c62cbcdb0cecaaf9c9ae88fd33e9ecd2d1
liyong <dixin_liyong@163.com>
2021-04-14 14:06:20 +0800

    codeing

---
+++ src/main/java/com/xor/learn/EvenTimesOddTimes.java
@@ -0,0 +1,68 @@
+package com.xor.learn;
+
+public class EvenTimesOddTimes {
+    public static int printOddNum(int[] arr) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        int xor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor ^= arr[i];
+        }
+        return xor;
+    }
+    public static int countOdd(int[] arr) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        int oneNum = arr[0];
+        int count = 0;
+        int otherNum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == oneNum) {
+                count ++;
+            } else {
+                otherNum = arr[i];
+            }
+        }
+        if (count % 2 == 0) {
+            return otherNum;
+        } else {
+            return oneNum;
+        }
+    }
+    public static int[] generator(int maxValue, int maxSize) {
+        int odd = (int) (Math.random() * maxValue);
+        int even = (int) (Math.random() * maxValue);
+        int len = (int) (Math.random() * maxSize);
+        len = (len % 2 == 0) ? len + 1 : len;
+        int oneNumTimes = (int) (Math.random() * len);
+        oneNumTimes = oneNumTimes % 2 == 0 ? oneNumTimes : oneNumTimes + 1;
+        int[] arr = new int[len];
+        for (int i = 0; i < oneNumTimes; i++) {
+            arr[i] = even;
+        }
+        for (int i = oneNumTimes; i < len; i++) {
+            arr[i] = odd;
+        }
+        return arr;
+    }
+    public static  void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int testTime = 10000000;
+       int maxValue = 1000;
+       int maxSize = 1000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generator(maxValue,maxSize);
+            if (countOdd(arr) != printOddNum(arr)) {
+                printArray(arr);
+                System.out.println("error!");
+            }
+        }
+        System.out.println("finsh!");
+    }
+}
--- src/main/java/com/xor/learn/XOR.java
+++ src/main/java/com/xor/learn/XOR.java
@@ -1,9 +1,26 @@
 package com.xor.learn;
 
 public class XOR {
-
+    /**
+     * 得到二进制
+     * @param value
+     * @return
+     */
+    public static String getBytes(int value) {
+        String byteStr = "";
+        for (int i = 31; i >= 0 ; i--) {
+            byteStr += ((value & 1 << i) == 0) ? '0' : '1';
+        }
+        return byteStr;
+    }
+    public static int getRightOne(int value) {
+        return value & (~value + 1);
+    }
     public static void main(String[] args) {
-        System.out.println(0);
-
+        String bytes = getBytes(5346);
+        String b = getBytes(5346 & (~5346 + 1));
+        System.out.println(bytes+ ":" + bytes.length());
+        System.out.println(Integer.toBinaryString(5346 & (~5346 + 1)));
+        System.out.println(b);
     }
 }
